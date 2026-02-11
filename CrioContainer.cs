b9a56744e4c3f87c4f5275020552ca0e58b62a6d
Ederson Junior <ed.dev46@gmail.com>
2026-02-11 11:27:12 -0300

    Implementing initial Try_catch

--- CrioContainer.cs
+++ CrioContainer.cs
@@ -1,6 +1,25 @@
 ﻿namespace BioTechVaultManager;
 
+using System.Linq;
+
 public class CrioContainer<T> where T : Specimen
 {
+    private List<T> container = new List<T>();
 
+    public void AddSpecimen(T specimen)
+    {
+        container.Add(specimen);
+    }
+
+    public T GetSpecimenByName(string name)
+    {
+        var specimen = container.FirstOrDefault(x => x.Name == name);
+        if (specimen == null)
+        {
+            throw new Exception("Specimen not found!");
+        } else
+        {
+            return specimen;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.ComponentModel;
+using BioTechVaultManager;
+
+CrioContainer<Virus> virusContainer = new CrioContainer<Virus>();
+
+try
+{
+        var ebola = virusContainer.GetSpecimenByName("ebola");
+} catch (Exception e)
+{
+        Console.WriteLine($"{e.Message}");
+}
+
+Console.ReadKey();
